b92515a237f4b2de22d45f6011108e4e8289f3bb
Vinny Diehl <vinny.diehl@gmail.com>
2023-06-23 04:57:12 -0400

    Add more tests for functions

--- src/functions.rs
+++ src/functions.rs
@@ -104,44 +104,151 @@ where T: FromCrispExpr + IntoCrispExpr + Copy {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::expr::CrispExpr::*;
+
+    macro_rules! num_list {
+        ($($elem:expr),*) => {
+            vec![ $( Number($elem) ),* ]
+        }
+    }
+
+    macro_rules! crisp_assert {
+        ($expr:expr) => {
+            assert_eq!($expr.unwrap(), Bool(true));
+        }
+    }
+
+    macro_rules! crisp_assert_false {
+        ($expr:expr) => {
+            assert_eq!($expr.unwrap(), Bool(false));
+        }
+    }
+
+    macro_rules! crisp_assert_eq {
+        ($expr:expr, $result:expr) => {
+            assert_eq!($expr.unwrap(), Number($result));
+        }
+    }
+
+    // Math
+
+    #[test]
+    fn test_add() {
+        crisp_assert_eq!(add(&num_list![6.0, 9.0]), 15.0);
+        crisp_assert_eq!(add(&num_list![1.0, 2.0, 3.0]), 6.0);
+    }
+
+    #[test]
+    fn test_sub() {
+        crisp_assert_eq!(sub(&num_list![6.0, 9.0]), -3.0);
+        crisp_assert_eq!(sub(&num_list![1.0, 2.0, 3.0]), -4.0);
+    }
+
+    #[test]
+    fn test_mult() {
+        crisp_assert_eq!(mult(&num_list![6.0, 9.0]), 54.0);
+        crisp_assert_eq!(mult(&num_list![5.0, 2.0, 3.0]), 30.0);
+    }
+
+    #[test]
+    fn test_div() {
+        crisp_assert_eq!(div(&num_list![9.0, 2.0]), 4.5);
+        crisp_assert_eq!(div(&num_list![30.0, 3.0, 2.0]), 5.0);
+    }
+
+    // Boolean
+
+    #[test]
+    fn test_eq() {
+        crisp_assert!(eq(&num_list![5.0, 5.0]));
+        crisp_assert!(eq(&num_list![30.0, 30.0, 30.0]));
+
+        crisp_assert_false!(eq(&num_list![5.0, 4.0]));
+        crisp_assert_false!(eq(&num_list![5.0, 4.0, 5.0]));
+    }
+
+    #[test]
+    fn test_gt() {
+        crisp_assert!(gt(&num_list![5.0, 4.0]));
+        crisp_assert!(gt(&num_list![4.0, 2.0, 0.0]));
+
+        crisp_assert_false!(gt(&num_list![5.0, 6.0]));
+        crisp_assert_false!(gt(&num_list![4.0, 2.0, 2.0]));
+    }
+
+    #[test]
+    fn test_gte() {
+        crisp_assert!(gte(&num_list![5.0, 4.0]));
+        crisp_assert!(gte(&num_list![4.0, 2.0, 0.0]));
+        crisp_assert!(gte(&num_list![4.0, 2.0, 2.0, 1.5]));
+
+        crisp_assert_false!(gte(&num_list![5.0, 6.0]));
+        crisp_assert_false!(gte(&num_list![5.0, 4.0, 2.5, 3.0]));
+    }
+
+    #[test]
+    fn test_lt() {
+        crisp_assert!(lt(&num_list![5.0, 6.0]));
+        crisp_assert!(lt(&num_list![4.0, 7.0, 10.0]));
+
+        crisp_assert_false!(lt(&num_list![5.0, 1.0]));
+        crisp_assert_false!(lt(&num_list![4.0, 5.0, 5.0]));
+    }
+
+    #[test]
+    fn test_lte() {
+        crisp_assert!(lte(&num_list![5.0, 6.0]));
+        crisp_assert!(lte(&num_list![4.0, 7.0, 10.0]));
+        crisp_assert!(lte(&num_list![4.0, 5.0, 5.0, 5.5]));
+
+        crisp_assert_false!(lte(&num_list![5.0, 1.0]));
+        crisp_assert_false!(lte(&num_list![5.0, 7.0, 8.0, 7.5]));
+    }
 
     #[test]
     fn test_extract_value() {
         // Test case with a valid number
-        let expr = CrispExpr::Number(42.0);
+        let expr = Number(42.0);
         let result = extract_value::<f64>(&expr);
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), 42.0);
 
         // Test case with a non-number
-        let expr = CrispExpr::Symbol("abc".to_string());
+        let expr = Symbol("abc".to_string());
         let result = extract_value::<f64>(&expr);
         assert!(matches!(result, Err(CrispError::Reason(_))));
     }
 
     #[test]
     fn test_extract_list() {
-        // Test case with valid numbers
+        // Mixed types should error
         let result = extract_list::<f64>(&vec![
-            CrispExpr::Number(1.0),
-            CrispExpr::Number(2.0),
-            CrispExpr::Number(3.0),
-        ]);
-        assert_eq!(result.unwrap(), vec![1.0, 2.0, 3.0]);
-
-        // Test case with mixed types
-        let result = extract_list::<f64>(&vec![
-            CrispExpr::Number(1.0),
-            CrispExpr::Number(2.0),
-            CrispExpr::Symbol("foo".to_string())
+            Number(1.0),
+            Number(2.0),
+            Symbol("foo".to_string())
         ]);
         assert!(matches!(result, Err(CrispError::Reason(_))));
 
-        // Test bools
-        let result = extract_list::<bool>(&vec![
-            CrispExpr::Bool(true),
-            CrispExpr::Bool(false)
-        ]);
+        // Numbers
+        let result = extract_list::<f64>(&num_list![1.0, 2.0, 3.0]);
+        assert_eq!(result.unwrap(), vec![1.0, 2.0, 3.0]);
+
+        // Bools
+        let result = extract_list::<bool>(&vec![Bool(true), Bool(false)]);
         assert_eq!(result.unwrap(), vec![true, false]);
+    }
+
+    #[test]
+    fn test_list_foldl() {
+        let list = num_list![3.0, 4.0, 2.0];
+        assert_eq!(list_foldl::<f64, f64>(&list, 8.0, |acc, n| acc - n).unwrap(),
+                   Number(-1.0));
+    }
+
+    #[test]
+    fn test_list_foldl1() {
+        let list = num_list![3.0, 4.0, 2.0];
+        assert_eq!(list_foldl1::<f64>(&list, |acc, n| acc * n).unwrap(),
+                   Number(24.0));
     }
 }
