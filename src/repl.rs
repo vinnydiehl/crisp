c1d8d65796754127946d31371d7711a1b5c7f4b0
Vinny Diehl <vinny.diehl@gmail.com>
2023-06-28 03:09:24 -0400

    Improve REPL
    Used Rustyline to implement readline features such as arrow keys and
    persistent history.

--- src/repl.rs
+++ src/repl.rs
@@ -1,15 +1,28 @@
-use crate::{CrispExpr, env::initialize_environment};
+use crate::{CrispExpr, env::initialize_environment, print_return, send};
 
-use std::{io::{ self, BufRead, Write }, collections::hash_map::Entry};
+use std::collections::hash_map::Entry;
 
-use crate::{print_return, send};
+use colored::*;
+use rustyline::{error::ReadlineError, DefaultEditor};
 
 /// The Read/Execute/Print Loop (REPL). Continually prompts the user for
 /// expressions, which it evaluates immediately and prints the return value,
 /// maintaining an environment so the user may execute a program line-by-line.
 pub fn run() {
-    let stdin = io::stdin();
-    let mut stdout = io::stdout();
+    // Find the directory that the executable is running in; this is
+    // where we will save the history file.
+    let binding = std::env::current_exe()
+        .unwrap()
+        .canonicalize()
+        .expect("The current executable should exist.");
+    let dir = binding.parent()
+        .expect("The current executable should be a file.")
+        .to_string_lossy()
+        .to_owned();
+    let history_file: &str = &format!("{}/repl_history", dir);
+
+    let mut rl = DefaultEditor::new().unwrap();
+    let _ = rl.load_history(history_file);
 
     let env = &mut initialize_environment();
 
@@ -24,30 +37,67 @@ pub fn run() {
                         *n += 1.0;
                         *n
                     },
+
                     _ => {
                         *value = CrispExpr::Number(0.0);
                         0.0
                     }
                 }
-            }
+            },
+
             Entry::Vacant(entry) => {
                 entry.insert(CrispExpr::Number(0.0));
                 0.0
             }
         };
 
-        print!("crisp:{:03}> ", repl_line_count);
-        stdout.flush().unwrap();
+        let readline = rl.readline(&format!("crisp:{:03}> ", repl_line_count));
+        match readline {
+            Ok(line) => {
+                let str = line.as_str();
 
-        let line = stdin.lock().lines().next().unwrap().unwrap();
+                match send(str.to_string(), env) {
+                    Ok(ret) => print_return(&ret),
+                    Err(e) => eprintln!("{}", e)
+                };
 
-        if line.trim() == "quit" {
-            break;
+                rl.add_history_entry(str).unwrap_or_else(|err| {
+                    // Couldn't add to history, warn and continue
+                    let message = match err {
+                        ReadlineError::Io(io_err) => io_err.kind().to_string(),
+                        _ => err.to_string()
+                    };
+
+                    eprintln!("{} {}",
+                        format!("[{}] Unable to add history entry:", "Warning".yellow()).bold(),
+                        message
+                    );
+
+                    true
+                });
+            },
+
+            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => break,
+
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
         }
 
-        match send(line, env) {
-            Ok(ret) => print_return(&ret),
-            Err(e) => eprintln!("{}", e)
-        }
+        rl.save_history(history_file).unwrap_or_else(|err| {
+            // Couldn't save history, warn and continue
+            let message = match err {
+                ReadlineError::Io(io_err) => io_err.kind().to_string(),
+                _ => err.to_string()
+            };
+
+            eprintln!("{} {}",
+                format!("[{}] Unable to save history entry:", "Warning".yellow()).bold(),
+                message
+            );
+
+            ()
+        });
     }
 }
